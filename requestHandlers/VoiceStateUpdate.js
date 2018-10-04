21ef6ae3492d794344c3193c347eacd46e69445d
112madgamer <ryleegeorge1@gmail.com>
2018-10-04 20:24:59 +1000

    Cleanup logging

--- requestHandlers/VoiceStateUpdate.js
+++ requestHandlers/VoiceStateUpdate.js
@@ -9,7 +9,7 @@ class VoiceStateUpdate extends RequestHandler {
   }
 
   async handle (event) {
-    this.log.debug('H-VSU', `VSU for ${event.guild_id} `)
+    this.log.debug('H-VSU', `VSU for guild ${event.guild_id} `)
     this.bot.voiceStateUpdate(event.shard_id, event)
     const queue = await this.lavalink.queues.get(event.guild_id)
     await queue.player.join(event.channel_id)
@@ -18,9 +18,10 @@ class VoiceStateUpdate extends RequestHandler {
     let index
     if (Array.isArray(players)) index = players.findIndex(player => player.guild_id === event.guild_id)
     if (((!players && !index) || index < 0) && event.channel_id) {
-      this.log.debug('H-VSU', `Adding player ${event.guild_id} `)
+      this.log.debug('H-VSU', `Adding player for ${event.guild_id} `)
       await this.cache.storage.upsert('players', [{ guild_id: event.guild_id, channel_id: event.channel_id }])
     } else if (players && typeof index !== 'undefined' && index >= 0 && !event.channel_id) {
+      this.log.debug('H-VSU', `Removing player for ${event.guild_id} `)
       players.splice(index, 1)
       if (players.length === 0) await this.cache.storage.delete('players')
       else await this.cache.storage.set('players', players)
