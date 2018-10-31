838c09e33f1066f18a1b2b8c353619a06ce8278e
112madgamer <ryleegeorge1@gmail.com>
2018-10-31 16:31:39 +1100

    Cleanup

--- index.js
+++ index.js
@@ -21,7 +21,6 @@ const gateway = new GhostGateway({
   eventPath: path.join(__dirname, './requestHandlers/')
 })
 gateway.log.mode = 1
-
 gateway.log.info('Gateway', `
    _____ _    _  ____   _____ _______
   / ____| |  | |/ __ \\ / ____|__   __|
@@ -39,52 +38,13 @@ gateway.log.info('Gateway', `Starting gateway ${gateway.id}`)
 gateway.initialize()
 gateway.on('error', error => gateway.log.error('ERROR', error))
 gateway.bot.on('error', error => gateway.log.error('ERROR', error))
-/* const object = { d:
-  { guild_id: '268807882059939840',
-    attachments: [],
-    author:
-     { avatar: 'f37ad54f3bd57c5848be2c14945c281a',
-       discriminator: '3460',
-       id: '167927608191746048',
-       username: '112' },
-    channel_id: '268807882059939840',
-    content: '==test',
-    edited_timestamp: null,
-    embeds: [],
-    id: '500220744777596930',
-    member:
-     { deaf: false,
-       joined_at: '2017-01-11T18:26:33.296000+00:00',
-       mute: false,
-       nick: null,
-       roles: [Array] },
-    mention_everyone: false,
-    mention_roles: [],
-    mentions: [],
-    nonce: '500220744119091200',
-    pinned: false,
-    timestamp: '2018-10-12T08:18:39.207000+00:00',
-    tts: false,
-    type: 0 },
-op: 0,
-s: 6,
-t: 'MESSAGE_CREATE',
-shard_id: 0 } */
+
 gateway.bot.on('ready', () => {
   gateway.log.info('Gateway', 'Connected to Discord gateway')
-  /*   let count = 0
-  setInterval(async () => {
-    console.log(`Sending number ${count}`)
-    object.d['nonce'] = count
-
-    gateway.workerConnector.sendToQueue(object)
-    count++
-  }, 20) */
   setInterval(
     async () => {
       const shards = []
       for (const shard in gateway.bot.shardManager.shards) {
-        // console.log({ shard_id: gateway.bot.shardManager.shards[shard].id, shard_status: gateway.bot.shardManager.shards[shard].connector.status, shard_event: gateway.bot.shardManager.shards[shard].connector.seq })
         shards[shard] = { shard_id: gateway.bot.shardManager.shards[shard].id, shard_status: gateway.bot.shardManager.shards[shard].connector.status, shard_event: gateway.bot.shardManager.shards[shard].connector.seq }
       }
       await gateway.cache.storage.set('shards', shards)
@@ -110,18 +70,6 @@ gateway.bot.on('disconnected', () => { gateway.log.info('Gateway', 'All shards d
 
 gateway.bot.on('event', event => {
   if(event.d.author){ if (event.d.author.bot || event.d.author.id === process.env.BOT_ID) { return }}
-  gateway.stats.increment('discordevent', 1, 1, [`shard:${event.shard_id}`, `event:${event.t}`], (error) => {
-    if (error) {
-      this.client.log.error('Gateway-event', error)
-    }
-  })
-  if (event.t !== 'PRESENCE_UPDATE') {
-    gateway.stats.increment('discordevent.np', 1, 1, [`shard:${event.shard_id}`, `event:${event.t}`], (error) => {
-      if (error) {
-        this.client.log.error('Gateway-event', error)
-      }
-    })
-  }
   switch (event.t) {
     case 'VOICE_SERVER_UPDATE':
       gateway.lavalink.voiceServerUpdate(event.d)
@@ -132,10 +80,16 @@ gateway.bot.on('event', event => {
       break
   }
   if (event.t !== 'PRESENCE_UPDATE') {
+    gateway.stats.increment('discordevent', 1, 1, [`shard:${event.shard_id}`, `event:${event.t}`], (error) => {
+      if (error) {
+        this.client.log.error('Gateway-event', error)
+      }
+    })
     gateway.log.debug(`EVENT-${event.shard_id}`, event.t)
-    if (event.d) { event.d['type'] = event.t }
-    if (event.d) { event.d['gateway'] = gateway.id }
-
+    if (event.d) { 
+      event.d['type'] = event.t 
+      event.d['gateway'] = gateway.id
+    }
     gateway.workerConnector.sendToQueue(event)
   }
 })
